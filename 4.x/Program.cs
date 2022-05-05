2f96ac9b6435e601f3acc8d71dbe4e0498afab63
Exreller <pipkonp@mail.ru>
2022-05-05 21:40:36 +0300

    Добавил код программы

--- 4.x/Program.cs
+++ 4.x/Program.cs
@@ -6,7 +6,33 @@ namespace _4.x
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+			Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+			var color = Console.ReadLine();
+
+			switch (color)
+			{
+				case "red":
+					Console.BackgroundColor = ConsoleColor.Red;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Your color is red!");
+					break;
+
+				case "green":
+					Console.BackgroundColor = ConsoleColor.Green;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Your color is green!");
+					break;
+
+				default:
+					Console.BackgroundColor = ConsoleColor.Cyan;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Your color is cyan!");
+					break;
+			}
+		}
     }
 }

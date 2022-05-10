3936550c4d19ad70056c8406c330f58de66f9213
Exreller <pipkonp@mail.ru>
2022-05-10 09:46:16 +0300

    Переместил вывод запроса действия внутрь циклов, добавил цикл do while со счётчиком по переменной t

--- 4.x/Program.cs
+++ 4.x/Program.cs
@@ -7,10 +7,10 @@ namespace _4.x
         static void Main(string[] args)
         {
             Console.WriteLine("Цикл for");
-            Console.WriteLine("Напишите свой любимый цвет на английском, с маленькой буквы");
+            
             for (int i = 0; i < 3; i++)
             {
-                
+                Console.WriteLine("Напишите свой любимый цвет на английском, с маленькой буквы");
                 Console.WriteLine(i);                
                 switch (Console.ReadLine())
                 {
@@ -46,12 +46,12 @@ namespace _4.x
                 }
 
             }
-            Console.WriteLine("Цикл while");
-            int k = 0;
-            Console.WriteLine("Напишите свой любимый цвет на английском, с маленькой буквы");
 
+            Console.WriteLine("Цикл while");
+            int k = 0;           
             while (k < 3)
-            {                
+            {
+                Console.WriteLine("Напишите свой любимый цвет на английском, с маленькой буквы");
                 Console.WriteLine(k);          
 
                 switch (Console.ReadLine())
@@ -90,7 +90,52 @@ namespace _4.x
 
                 k++;
             }
-                   
+
+            Console.WriteLine("Цикл do while");
+            int t = 0;
+            do
+            {
+                Console.WriteLine("Напишите свой любимый цвет на английском, с маленькой буквы");
+                Console.WriteLine(t);
+
+                switch (Console.ReadLine())
+                {
+
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Your color is Yellow!");
+                        break;
+
+                }
+
+                t++;
+            }
+            while (t < 3);
+
         }
 		
     }

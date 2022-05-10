80ed2c8b1a76cec6a729ac9c0f7c672e06e20e10
Exreller <pipkonp@mail.ru>
2022-05-10 09:37:20 +0300

    Поправил циклы к заданию 4.2.11

--- 4.x/Program.cs
+++ 4.x/Program.cs
@@ -6,10 +6,53 @@ namespace _4.x
     {
         static void Main(string[] args)
         {
-            int i = 0;
-            while (true)
+            Console.WriteLine("Цикл for");
+            Console.WriteLine("Напишите свой любимый цвет на английском, с маленькой буквы");
+            for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine(i++);
+                
+                Console.WriteLine(i);                
+                switch (Console.ReadLine())
+                {
+
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Your color is Yellow!");
+                        break;
+                }
+
+            }
+            Console.WriteLine("Цикл while");
+            int k = 0;
+            Console.WriteLine("Напишите свой любимый цвет на английском, с маленькой буквы");
+
+            while (k < 3)
+            {                
+                Console.WriteLine(k);          
 
                 switch (Console.ReadLine())
                 {
@@ -42,8 +85,10 @@ namespace _4.x
 
                         Console.WriteLine("Your color is Yellow!");
                         break;
+
                 }
 
+                k++;
             }
                    
         }
